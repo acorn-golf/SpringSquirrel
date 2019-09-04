0b3cff27f8ac58fa61316a5d1089d2872cbd4741
khs10005 <khs10005@naver.com>
2019-09-04 20:55:19 +0900

    until adminSelect

--- src/main/java/com/squirrel/controller/AdminController.java
+++ src/main/java/com/squirrel/controller/AdminController.java
@@ -24,66 +24,100 @@ public class AdminController {
 	@Autowired AdminService aService;	
 	@Autowired MemberService mService;	
 	
-	@RequestMapping(value="/adminPage", method=RequestMethod.POST)
+	@RequestMapping(value="/adminPage")
 	public String adminPage(HttpSession session, HttpServletRequest request, 
 									@RequestParam HashMap<String, Object> map) {
 		
 		MemberDTO mDTO = (MemberDTO)session.getAttribute("login");
 		String destination = null;
-
-		if( !mDTO.getRating().equals("A")) {
-			session.invalidate();
-			destination = "redirect:member/login";			
-		}else {
-			int user_no = mDTO.getUser_no();			
-			mDTO = mService.myPage(user_no);
+	
+		if( !mDTO.getRating().equals("A") ) {
 			
-			String curPage = null;
+			session.invalidate();
+			destination = "redirect:member/loginForm";	
+			
+		}else {
+			
+			int user_no = mDTO.getUser_no();			
+			mDTO = mService.myPage(user_no);			
+			
+			String curPageStr = (String)map.get("curPage");
 			String adminSelect = (String)map.get("adminSelect");
 			String adminSearch = (String)map.get("adminSearch");
 			
-			if( map.get("curPage") == null ) { curPage = "1"; }
+			System.out.println(">>>>"+adminSearch);
+			if( curPageStr == null ) { 
+				curPageStr = "1"; 
+			}
 			
-			int perPage = 10;
-			int start = perPage * ((Integer.parseInt(curPage))-1)+1 ;
-			int end = perPage-1 + start;
+			int curPage = Integer.parseInt(curPageStr);				
+			int perPage = 10; // 페이지당 보여줄 개수
+			int start = perPage * ( (curPage) - 1 ) + 1; // Rownum에 사용될 시작 값
+			int end = perPage - 1 + start; // Rownum에 사용될 끝 값
 			int totalRecord = 0;
 			
 			map.put("start", start);
 			map.put("end", end);
 			
-			switch (adminSelect) {
-				
+			switch ( adminSelect ) {
+			
 				case "member" : 
-					List<MemberDTO> mList = aService.adminMemberSelect(map);
-					totalRecord = aService.totalRecord(map);
+					List<MemberDTO> mList = aService.adminMemberSelect(map);			
 					request.setAttribute("mList", mList);
 					break;
 					
 				case "product" :
-					List<ProductDTO> pList = aService.adminProductSelect(map);
-					totalRecord = aService.totalRecord(map);
+					List<ProductDTO> pList = aService.adminProductSelect(map);					
 					request.setAttribute("pList", pList);
 					break;
 					
 				case "golfcc" :
-					List<GolfCcDTO> gList = aService.adminGolfCcSelect(map);
-					totalRecord = aService.totalRecord(map);
+					List<GolfCcDTO> gList = aService.adminGolfCcSelect(map);			
 					request.setAttribute("gList", gList);
 					break;
 			}
-		
+			
+			totalRecord = aService.totalRecord(map);
+			
+			int endPage = totalRecord / perPage; // 마지막 페이지			
+			if( totalRecord % perPage != 0 ) {
+				endPage++; // 페이지가 모자를 경우 endPage를 올림한당.
+			}
+			
+			int showPage = 10; // 한번에 보여줄 페이지
+			int startPage = (curPage-1) / showPage * showPage + 1; // 첫장은 1 두번짼 2
+			int lastPage = startPage + showPage - 1; // 첫장은 10, 두번짼 20			
+			if( curPage == endPage || endPage < startPage + showPage ) {
+				lastPage = endPage;
+			}else if (curPage < endPage) {
+				lastPage = ( startPage + showPage ) - 1;
+			}
+			
+			int beforeShow = curPage - showPage;			
+			if( beforeShow < 1 ) {
+				beforeShow = 1; // 0 는 음수값이 될 경우
+			}
+			
+			int afterShow = beforeShow + showPage;			
+			if( afterShow > totalRecord ) {
+				afterShow = totalRecord; // 총 게시물을 초과할 경우
+			}
+			
 			request.setAttribute("adminSelect", adminSelect);
 			request.setAttribute("adminSearch", adminSearch);
+			request.setAttribute("curPage", curPage);
+			request.setAttribute("endPage", endPage);
+			request.setAttribute("totalRecord", totalRecord);
+			request.setAttribute("showPage", showPage);
+			request.setAttribute("startPage", startPage);
+			request.setAttribute("lastPage", lastPage);
+			request.setAttribute("beforeShow", beforeShow);
+			request.setAttribute("afterShow", afterShow);		
 			
-			
-			
-			
-			
+			session.setAttribute("login", mDTO);
 			
 			destination = "member/adminPageResult";
-		}
-		
+		}		
 		
 		return destination;
 	}
